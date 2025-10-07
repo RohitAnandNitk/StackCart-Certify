51caa4a996247c604659a7a5dd7665f92e91060a
RohitAnandNitk <sharmarohit64748@gmail.com>
2025-10-07 23:48:29 +0530

    frontend url updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,7 +23,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173", // local dev frontend
-      "https://stackcart-certify.vercel.app", // production frontend
+      "https://www.stackcart.space", // production frontend
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
